ba287026771f7a124aa97c02eb843f84edefd283
Faijan Mansuri <faijanmansuri95@gmail.com>
2026-06-05 12:37:12 +0530

    Test Student Controller

--- StudentManagementSystem/Controllers/StudentController.cs
+++ StudentManagementSystem/Controllers/StudentController.cs
@@ -1,77 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using StudentManagementSystem.Data;
-using StudentManagementSystem.Models;
 
 namespace StudentManagementSystem.Controllers
 {
     public class StudentController : Controller
     {
-        private readonly ApplicationDbContext _context;
-
-        public StudentController(ApplicationDbContext context)
+        public IActionResult Index()
         {
-            _context = context;
-        }
-
-        public async Task<IActionResult> Index()
-        {
-            var students = await _context.Students.ToListAsync();
-            return View(students);
-        }
-
-
-
-        public async Task<IActionResult> Edit(int id)
-        {
-            var student = await _context.Students.FindAsync(id);
-
-            if (student == null)
-            {
-                return NotFound();
-            }
-
-            return View(student);
-        }
-
-
-    
-        [HttpPost]
-        public async Task<IActionResult> Edit(Student student)
-        {
-            _context.Students.Update(student);
-            await _context.SaveChangesAsync();
-
-            return RedirectToAction("Index");
-        }
-
-
-
-        public async Task<IActionResult> Delete(int id)
-        {
-            var student = await _context.Students.FindAsync(id);
-
-            if (student != null)
-            {
-                _context.Students.Remove(student);
-                await _context.SaveChangesAsync();
-            }
-
-            return RedirectToAction("Index");
-        }
-
-
-        public IActionResult Create()
-        {
-            return View();
-        }
-        [HttpPost]
-        public async Task<IActionResult> Create(Student student)
-        {
-            _context.Students.Add(student);
-            await _context.SaveChangesAsync();
-
-            return RedirectToAction("Index");
+            return Content("Student Controller Working Successfully");
         }
     }
 }
