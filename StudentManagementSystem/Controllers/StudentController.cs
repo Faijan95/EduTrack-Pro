94a73c05881d80ccb874828020db7f7795a86871
Faijan Mansuri <faijanmansuri95@gmail.com>
2026-06-05 12:51:12 +0530

    Fix student page for deployment

--- StudentManagementSystem/Controllers/StudentController.cs
+++ StudentManagementSystem/Controllers/StudentController.cs
@@ -1,12 +1,76 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentManagementSystem.Data;
+using StudentManagementSystem.Models;
 
 namespace StudentManagementSystem.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public StudentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return Content("Student Controller Working Successfully");
+            return View(new List<Student>());
+        }
+
+
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+
+    
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student student)
+        {
+            _context.Students.Update(student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+
+            if (student != null)
+            {
+                _context.Students.Remove(student);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Student student)
+        {
+            _context.Students.Add(student);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
         }
     }
 }
