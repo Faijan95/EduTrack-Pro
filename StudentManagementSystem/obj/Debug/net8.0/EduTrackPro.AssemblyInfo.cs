acfe715a2a46c8f7c88e21308439c939164c7ef2
Faijan Mansuri <faijanmansuri95@gmail.com>
2026-06-05 11:59:31 +0530

    Add Dockerfile for Render

--- StudentManagementSystem/obj/Debug/net8.0/EduTrackPro.AssemblyInfo.cs
+++ StudentManagementSystem/obj/Debug/net8.0/EduTrackPro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EduTrackPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f25db601c16b865557e21cb0c03c364cbb6b57d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c9fd350d4949049f3377298507ec1d5fcaa45d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EduTrackPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EduTrackPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
